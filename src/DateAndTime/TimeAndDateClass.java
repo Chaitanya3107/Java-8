3ad88bde19ed2fe95a468ed0eb7f4bc811e4fc4a
Chaitanya_Anchal <chaitanyaanchal3107@gmail.com>
2025-12-18 20:29:39 +0530

    studied about local date time class

--- src/DateAndTime/TimeAndDateClass.java
+++ src/DateAndTime/TimeAndDateClass.java
@@ -8,6 +8,10 @@ public class TimeAndDateClass {
         System.out.println(now);
         LocalDateTime parsedDateTime = LocalDateTime.parse("2025-07-31T10:50");
         System.out.println(parsedDateTime);
+        LocalDateTime localDateTime = parsedDateTime.minusDays(15);
+        System.out.println(localDateTime);
+        LocalDateTime localDateTime1 = parsedDateTime.minusHours(2);
+        System.out.println(localDateTime1);
 
     }
 }
